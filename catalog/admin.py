32d7066cbb3c2384d505871f544651f1cc51fd7d
Pham Viet Anh <166052827+PhamVanh04@users.noreply.github.com>
2025-07-15 18:12:44 +0700

    Django Part 4: Django admin site
    Django Part 4: Django admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Genre, Book, BookInstance
+
+
+
+
+admin.site.register(Genre)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Định nghĩa lớp admin
+
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BookInstanceInline]
+
+
+
+# Đăng ký lớp admin với model tương ứng
+
+admin.site.register(Book, BookAdmin)
+
+
+
+class AuthorAdmin(admin.ModelAdmin):
+
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+
+# Đăng ký lớp admin với model tương ứng
+
+admin.site.register(Author, AuthorAdmin)
+
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+
+        (None, {
+
+            'fields': ('book', 'imprint', 'id')
+
+        }),
+
+        ('Availability', {
+
+            'fields': ('status', 'due_back')
+
+        }),
+
+    )
+
+
+
+admin.site.register(BookInstance, BookInstanceAdmin)
