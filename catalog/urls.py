fa3e660e002151fe2f1be34c782abd05a1653c6a
Pham Viet Anh <166052827+PhamVanh04@users.noreply.github.com>
2025-07-21 14:51:30 +0700

    Django tutorial part10: Testing a django web application

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,8 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Trang chủ
     path('', views.index, name='index'),
-    path('books/', views.BookListView.as_view(), name='books'),
-    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 
+    # Danh sách & chi tiết sách
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+
+    # Danh sách sách mà user đang mượn
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    # CRUD tác giả
+    path('author/create/', views.AuthorCreate.as_view(),   name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+
+    # Gia hạn sách (thủ thư)
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
+
--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,19 @@ from .models import Book, Author, BookInstance, Genre
 from . import constants
 from django.views import generic
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.contrib.auth.decorators import permission_required
+
+import datetime
+from datetime import date
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.forms import RenewBookForm
+
 def index(request):
     """Hàm view cho trang chủ của trang web."""
 
@@ -25,19 +38,22 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # 3. Kết xuất (render) template HTML và gửi về cho người dùng
     return render(request, 'index.html', context=context)
 
-class BookListView(generic.ListView):
+class BookListView(PermissionRequiredMixin,LoginRequiredMixin,generic.ListView):
     model = Book
     paginate_by = constants.BOOK_LIST_VIEW_PAGINATE
     context_object_name = 'book_list'
     template_name = 'catalog/book_list.html'
+    permission_required = 'catalog.can_see_all_books'
 
-class BookDetailView(generic.DetailView):
+class BookDetailView(PermissionRequiredMixin,LoginRequiredMixin,generic.DetailView):
     model = Book
+    permission_required = 'catalog.can_see_all_books'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -51,3 +67,54 @@ class BookDetailView(generic.DetailView):
 def book_detail_view(request, primary_key):
     book = get_object_or_404(Book, pk=primary_key)
     return render(request, 'catalog/book_detail.html', context={'book': book})
+
+class MyView(LoginRequiredMixin, View):
+    login_url = '/login/'
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+    redirect_field_name = 'redirect_to'
+=======
+=======
+>>>>>>> e97936a (Django tutorial part9: Workings with forms)
+    redirect_field_name = 'redirect_to'
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+        form = RenewBookForm(request.POST)
+
+        if form.is_valid():
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': constants.DEFAULT_DATE_OF_DEATH}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
